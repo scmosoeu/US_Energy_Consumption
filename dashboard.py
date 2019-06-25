3afe98d58b653a1ef839919cf930c0766187694c
Selebogo <scmosoeu@gmail.com>
2019-06-25 09:21:59 +0200

    Initial launch

--- dashboard.py
+++ dashboard.py
@@ -22,6 +22,8 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__,external_stylesheets=external_stylesheets)
 app.config['suppress_callback_exceptions'] = True
 
+server = app.server
+
 app.layout = html.Div([
     html.Div([
 
